e1fe6d72923e5e9c445e9137a7ce8b7e7131cc76
Pavel Sumbaev <p-sumbaev@yandex.ru>
2021-08-09 19:43:32 +0300

    fix eslint

--- src/components/common/Slider/Slider.jsx
+++ src/components/common/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import SwiperCore, { Navigation, Pagination, Scrollbar } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { SwiperWrapper } from './styled';
@@ -7,21 +7,29 @@ SwiperCore.use([Navigation, Pagination, Scrollbar]);
 
 function Slider({ children, overflow }) {
   const [slides, setSlides] = useState();
+  const sliderRef = useRef();
   useEffect(() => {
     setSlides(Array.isArray(children) ? children : [children]);
   }, [children]);
+
+  useEffect(() => {
+    sliderRef.current.scrollbar.updateSize();
+  }, [slides]);
+
 
   return (
     <SwiperWrapper overflow={overflow}>
       <Swiper
         pagination={{ clickable: true }}
+        onInit={(ref)=>{sliderRef.current = ref;
+        }}
         navigation={{
           nextEl: '.swiper-button-next-unique',
           prevEl: '.swiper-button-prev-unique',
         }}
         spaceBetween={20}
         slidesPerView={1}
-        scrollbar={{ draggable: true }}
+        scrollbar={{ draggable: true, hide: true }}
         breakpoints={{
           480: {
             slidesPerView: 2,
